23a3f339d57fe060a34f5df2320e1ed6d1dd1c07
shnrndk <shnrndk@gmail.com>
2020-08-04 17:36:04 +0530

    db storing completed

--- Components/home.js
+++ Components/home.js
@@ -1,14 +1,56 @@
 const { disableExpoCliLogging } = require("expo/build/logs/Logs")
 import * as firebase from 'firebase';
-import React, { useState } from 'react';
+import React, { useState,useEffect,useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
+import { GiftedChat } from 'react-native-gifted-chat'
+
+function getuser(){
+  return (firebase.auth().currentUser || {}).uid;
+}
+
 
 export default function Home() {
+
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        setMessages([
+          {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+        ])
+      }, [])
+
+      const onSend = useCallback(async (messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+        
+
+        let msg = {
+            txt:messages[0]['text'],
+            user:await getuser(),
+            timestamp:firebase.database.ServerValue.TIMESTAMP
+        }
+        
+        
+        firebase.database().ref('messages').push(msg)
+      }, [])
+
     return (
-        <View>
-            <Button mode="contained" color="#841584" style={styles.btntrig} onPress={() => { firebase.auth().signOut() }}>Logout</Button>
-        </View>
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: getuser(),
+            }}
+        />
     )
 }
 
